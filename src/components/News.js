049e12b85fad8884121ed717f562b6bb9888e61e
dipesh maharjan <max.maharjan.2014@gmail.com>
2022-06-30 21:47:13 +0545

    api managed

--- src/components/News.js
+++ src/components/News.js
@@ -6,16 +6,16 @@ import Spinner from './Spinner';
 // import {spinner} from 'react-loader-spinner'
 
 
-const News = ({ api_Key,country,category,pageSize ,setProgress}) => {
+const News = ({ api_Key, country, category, pageSize, setProgress }) => {
     const [resp, setResp] = useState([]);
     const [totalResults, setTotalResults] = useState(0)
     const [page, setPage] = useState(1)
-    const [loading,setLoading] =useState(true)
+    const [loading, setLoading] = useState(true)
 
     const URL = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${api_Key}&page=${page}&pageSize=${pageSize}`;
 
-    const capFirstLetter =(string)=>{
-        return string.charAt(0).toUpperCase()+string.slice(1)
+    const capFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1)
     }
 
 
@@ -23,7 +23,7 @@ const News = ({ api_Key,country,category,pageSize ,setProgress}) => {
         setLoading(true)
         setProgress(10)
         await axios.get(URL)
-        .then((res) => {
+            .then((res) => {
                 // console.log(res.data)
                 setResp(res.data.articles)
                 setTotalResults(res.data.totalResults)
@@ -38,7 +38,7 @@ const News = ({ api_Key,country,category,pageSize ,setProgress}) => {
     }, [])
 
     const fetchMoreData = async () => {
-        const URL = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${api_Key}&page=${page+1}&pageSize=${pageSize}`;
+        const URL = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${api_Key}&page=${page + 1}&pageSize=${pageSize}`;
         setPage(page + 1);
         await axios.get(URL)
             .then((res) => {
@@ -51,30 +51,31 @@ const News = ({ api_Key,country,category,pageSize ,setProgress}) => {
 
     return (
         <>
-            <h1 style ={{marginTop : '100px'}}>NEWS TODAY - TOP {capFirstLetter(category)} Headlines </h1>
-            {loading && <Spinner/>}
-            <InfiniteScroll
-                dataLength={resp.length}
-                // height={3}
-                next={fetchMoreData}
-                hasMore={resp.length !== totalResults}
-                loader={<Spinner/>}
-            >
-                <div className='container my-4'>
+            <div className="App">
+                <h1 style={{ marginTop: '100px' }}>NEWS TODAY - TOP {capFirstLetter(category)} Headlines </h1>
+                {loading && <Spinner />}
+                <InfiniteScroll
+                    dataLength={resp.length}
+                    // height={3}
+                    next={fetchMoreData}
+                    hasMore={resp.length !== totalResults}
+                    loader={<Spinner />}
+                >
+                    <div className='container my-4'>
 
-                    <div className="row my-4">
-                        {resp.map((res,index) => {
-                            return (
-                                <div className='col-sm' key={res.url}>
-                                    <NewsItems imgUrl={res.urlToImage} title={res.title} url={res.url} description={res.description} author ={res.author} date={res.publishedAt} source={res.source.name}/>
-                                </div>
+                        <div className="row my-4">
+                            {resp.map((res, index) => {
+                                return (
+                                    <div className='col-sm' key={res.url}>
+                                        <NewsItems imgUrl={res.urlToImage} title={res.title} url={res.url} description={res.description} author={res.author} date={res.publishedAt} source={res.source.name} />
+                                    </div>
 
-                            )
-                        })}
+                                )
+                            })}
+                        </div>
                     </div>
-                </div>
-            </InfiniteScroll>
-
+                </InfiniteScroll>
+            </div>
         </>
     )
 }
